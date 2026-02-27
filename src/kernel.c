1f5125788f12ae14190d8b773649b961bbe540e3
Tenzin Kalsang <kalsangkalsang5@gmail.com>
2026-02-27 11:02:09 +0530

    kernel.c update

--- src/kernel.c
+++ src/kernel.c
@@ -1,7 +1,7 @@
 void kernel_main() {
-short video_memory = (short)0xB8000;
-const char* str = "Welcome to My OS - Seminar Project";
-for (int i = 0; str[i] != '\0'; i++) {
-video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
-}
+    short* video_memory = (short*)0xB8000;
+    const char* str = "Welcome to My OS - Seminar Project";
+    for (int i = 0; str[i] != '\0'; i++) {
+        video_memory[i] = (short)str[i] | 0x0F00;
+    }
 }
