c9378e20d6f8859687cd34bcb7364cfd0c5344d9
Tenzin Kalsang <kalsangkalsang5@gmail.com>
2026-02-28 21:28:26 +0530

    cros compiler compat headers re

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,17 +1,16 @@
-/*check if the compiler is targeting the wrong operating system*/
-#if defined(__linux__)
-#error "You are not using a cross-compiler, you will run into trouble"
-#endif
+#include <stdint.h>
+#include <stddef.h>
+
 
 void kernel_main(void) 
 {
-	terminal_buffer = (uint16_t*) 0xB8000;
+    uint16_t* terminal_buffer = (uint16_t*) 0xB8000;
 
-	const char* str = "Welcome to TenzinOs!";
-	
-	for (int i = 0; str[i] != '\0'; i++) {
-		terminal_buffer[i] = (uint16_t) str[i] | (uint16_t) 0x07 << 8;
-	}
+    const char* str = "Welcome to TenzinOs!";
+    
+    for (int i = 0; str[i] != '\0'; i++) {
+        terminal_buffer[i] = (uint16_t) str[i] | (uint16_t) 0x07 << 8;
+    }
 
-	while (1);
+    while (1);
 }
